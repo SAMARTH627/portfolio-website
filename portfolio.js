913b2542346ae25b352897a7e73a8aec25c42121
SAMARTH627 <111736287+SAMARTH627@users.noreply.github.com>
2023-11-16 22:18:56 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+// Add JavaScript functionality as needed
+document.addEventListener("DOMContentLoaded", function () {
+    const contactForm = document.getElementById("contact-form");
+
+    contactForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+        // Add code to handle form submission here
+        alert("Form submitted!");
+    });
+});
